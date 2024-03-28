a3152ad06865cfe1209ce76411bb9a1948429657
thilgesjoa <162282864+thilgesjoa@users.noreply.github.com>
2024-03-28 03:57:11 +0100

    Create charset.js

---
+++ charset.js
@@ -0,0 +1,13 @@
+function generateRandomString(length) {
+    const charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let randomString = '';
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * charset.length);
+        randomString += charset[randomIndex];
+    }
+    return randomString;
+}
+
+// Generate a random string with a length of 8 characters
+const randomString = generateRandomString(8);
+console.log("Random string:", randomString);
