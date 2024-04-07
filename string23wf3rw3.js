e46e4590dcff0088ac033f787b0bd4cc34df0ced
thilgesjoa <162282864+thilgesjoa@users.noreply.github.com>
2024-04-07 05:22:48 +0200

    Create string23wf3rw3.js

---
+++ string23wf3rw3.js
@@ -0,0 +1,8 @@
+public class StringConcatenation {
+    public static void main(String[] args) {
+        String str1 = "Hello";
+        String str2 = "World";
+        String result = str1.concat(" ").concat(str2);
+        System.out.println("Concatenated string: " + result);
+    }
+}
