e6694b542c89a94e09cc0f5f28e536ff4035a7bc
thilgesjoa <162282864+thilgesjoa@users.noreply.github.com>
2024-04-09 05:04:35 +0200

    Create agsrded.js

---
+++ agsrded.js
@@ -0,0 +1,19 @@
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void displayInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+
+    public static void main(String[] args) {
+        Person person = new Person("Alice", 30);
+        person.displayInfo();
+    }
+}
