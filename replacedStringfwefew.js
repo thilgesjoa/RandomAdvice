2c5204d4b8f25c552cc74a71cba361c47c0ddebd
thilgesjoa <162282864+thilgesjoa@users.noreply.github.com>
2024-04-10 05:21:21 +0200

    Create replacedStringfwefew.js

---
+++ replacedStringfwefew.js
@@ -0,0 +1,7 @@
+public class StringReplaceExample {
+    public static void main(String[] args) {
+        String str = "Hello, World!";
+        String replacedString = str.replace('o', 'X');
+        System.out.println("Replaced string: " + replacedString);
+    }
+}
