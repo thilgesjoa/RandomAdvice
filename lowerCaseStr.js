ec1d2d83e2c7c375e8d9d9342eff85ba1a4f015f
thilgesjoa <162282864+thilgesjoa@users.noreply.github.com>
2024-04-13 05:34:36 +0200

    Create lowerCaseStr.js

---
+++ lowerCaseStr.js
@@ -0,0 +1,7 @@
+public class StringToLowerCase {
+    public static void main(String[] args) {
+        String str = "Hello, World!";
+        String lowerCaseStr = str.toLowerCase();
+        System.out.println("Lower case string: " + lowerCaseStr);
+    }
+}
