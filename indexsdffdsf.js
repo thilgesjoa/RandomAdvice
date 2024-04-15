ad09ac74fbd12261f8ae24d5e69903bc7befd7ed
thilgesjoa <162282864+thilgesjoa@users.noreply.github.com>
2024-04-15 05:01:48 +0200

    Create indexsdffdsf.js

---
+++ indexsdffdsf.js
@@ -0,0 +1,7 @@
+public class CharAtExample {
+    public static void main(String[] args) {
+        String str = "Hello";
+        char ch = str.charAt(2);
+        System.out.println("Character at index 2: " + ch);
+    }
+}
