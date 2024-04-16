8c643a51a0bda634759ac8d7ba66cf1166da3208
thilgesjoa <162282864+thilgesjoa@users.noreply.github.com>
2024-04-16 05:44:11 +0200

    Create Valueasdfasd.js

---
+++ Valueasdfasd.js
@@ -0,0 +1,9 @@
+public class DoWhileLoop {
+    public static void main(String[] args) {
+        int i = 1;
+        do {
+            System.out.println("Value of i: " + i);
+            i++;
+        } while (i <= 5);
+    }
+}
