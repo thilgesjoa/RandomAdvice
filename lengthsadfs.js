785823d69e9d2716be3df8e30c56afa384186095
thilgesjoa <162282864+thilgesjoa@users.noreply.github.com>
2024-04-24 05:28:29 +0200

    Create lengthsadfs.js

---
+++ lengthsadfs.js
@@ -0,0 +1,10 @@
+public class SumOfMainDiagonal {
+    public static void main(String[] args) {
+        int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        int sum = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            sum += matrix[i][i];
+        }
+        System.out.println("Sum of main diagonal: " + sum);
+    }
+}
