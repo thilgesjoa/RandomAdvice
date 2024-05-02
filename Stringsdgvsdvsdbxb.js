e2d4baa7fc14e40218ce018e132cf24f336f1bff
thilgesjoa <162282864+thilgesjoa@users.noreply.github.com>
2024-05-02 05:35:50 +0200

    Create Stringsdgvsdvsdbxb.js

---
+++ Stringsdgvsdvsdbxb.js
@@ -0,0 +1,7 @@
+public class MaxOfTwoNumbers {
+    public static void main(String[] args) {
+        int a = 10, b = 20;
+        int max = (a > b) ? a : b;
+        System.out.println("Maximum of " + a + " and " + b + " is " + max);
+    }
+}
